d95e110ea0351b1275ad75f731d75b5a84a61d8e
Harikrishnan s <harikrishnansivadasan@gmail.com>
2025-05-15 17:21:18 +0530

    pdfloader-added

--- app.py
+++ app.py
@@ -1,3 +1,20 @@
 import streamlit as st
-from modules.parser import resume_parser
+from modules.fileloader import pdfloader
 
+st.title("LetterForge")
+st.subheader("Create a Perfect cover letter for your job application")
+
+resume = st.sidebar.file_uploader(
+    "Upload your resume", type=["pdf", "docx"], key="resume"
+)
+job_description = st.text_area("Paste your job description here", height=300)
+Generate = st.button("Generate Cover Letter")
+
+if Generate:
+    if resume is not None:
+        with st.spinner("processing.."):
+            resume_content = pdfloader.load_pdf(resume)
+            st.success("Resume loaded successfully!")
+            st.write(resume_content)
+    else:
+        st.error("Please upload a resume file.")

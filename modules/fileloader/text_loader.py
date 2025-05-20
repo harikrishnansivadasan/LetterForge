50e67640a15d1f10c802b3024adaa7fb7b278fc1
Harikrishnan s <harikrishnansivadasan@gmail.com>
2025-05-21 01:27:17 +0530

    comments_style

--- modules/fileloader/text_loader.py
+++ modules/fileloader/text_loader.py
@@ -14,23 +14,23 @@ def load_text(input_text):
         if not input_text:
             return ""
 
-        """strip whitespace"""
+        # strip whitespace
         text = input_text.strip()
 
-        """Remove non-printable characters"""
+        # Remove non-printable characters
         printable = set(string.printable)
         text = "".join(filter(lambda x: x in printable, text))
 
-        """Replace multiple spaces with single space"""
+        # Replace multiple spaces with single space
         text = re.sub(r"[ \t]+", " ", text)
 
-        """Replace multiple newlines with a single newline"""
+        # Replace multiple newlines with a single newline
         text = re.sub(r"\n+", "\n", text)
 
-        """Remove leading/trailing spaces on each line"""
+        # Remove leading/trailing spaces on each line
         text = "\n".join(line.strip() for line in text.split("\n"))
 
-        """Remove empty lines"""
+        #Remove empty lines
         text = "\n".join(line for line in text.split("\n") if line.strip())
 
         return text
